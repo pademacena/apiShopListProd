60bd70e408b29f3fb599a5c4c1803b4bb7226de6
pademacena <pademacena@gmail.com>
2019-10-11 21:25:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/atuthControlelrs')(app);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
